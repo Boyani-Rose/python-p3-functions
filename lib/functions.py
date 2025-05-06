46dfec56310c5e28e99cca414cca53a4ba6a00ca
Rose Momanyi <rosebmomanyi@gmail.com>
2025-05-06 13:18:32 +0300

    Completed the lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    name = "Guido"
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("Sunny")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+sum_result = add(45, 55)
+print(sum_result)
+    
 
 def halve(number):
-    pass
+    
+    return number / 2
+result_halving = halve(8)
+print(result_halving)
